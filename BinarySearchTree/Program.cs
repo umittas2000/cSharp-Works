9756fe653918e39e861e8dd86a0794b6c4f618d8
Umit <umittas2000@gmail.com>
2022-09-10 01:17:56 -0300

    Add files via upload

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BinarySearchTreeSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[10] { 7, 5, 1, 8, 3, 6, 0, 9, 4, 2 };
+
+            int sira = 0;
+            int root = 0;
+            int sol = 0;
+            int sag = 0;
+            int test = 0;
+            int pointer = 0;
+
+            for(int i = 0; i < dizi.Length; i++)
+            {
+                if (i == 0)
+                {
+                    root = dizi[i];
+                }
+                else
+                {
+                    if (root > dizi[i])
+                    {
+                        Console.WriteLine(sira +". seviye "+ root +"<<: " + dizi[i]);
+                    }
+
+                    if (root < dizi[i])
+                    {
+                        Console.WriteLine(sira +". seviye >>: " + dizi[i]);
+                    }
+
+                    sira++;
+                }
+
+               // Console.WriteLine(i + " . Dizi elemani: " + dizi[i]);
+            }
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace insertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  a = 0;
+            Console.Write("How many numbers do you want to give ?:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int[] myArr = new int[a];
+            //{ 22, 27, 16, 2, 18, 6 };
+
+            string input="";
+           for(int mm = 0;mm<a;mm++)
+            {
+                Console.Write("Please enter " + (mm+1) + ". Number(s): ");
+                input = Console.ReadLine();
+
+                myArr[mm] = Convert.ToInt32(input);
+            }
+
+            for(int i = 0; i < myArr.Length; i++)
+            {
+                int num = myArr[i];
+              
+                for(int t=i+1;t<myArr.Length;t++)
+                {
+                    int num2 = myArr[t];
+
+                    if (num > num2)
+                    {
+                        myArr[i] = num2;
+                        myArr[t] = num;
+                        i--;
+                        break;
+                    }
+
+                }
+
+            }
+
+            foreach(int item in myArr)
+            {
+                Console.WriteLine("Insertion Sort:" + item);
+            }
+        }
+    }
+}
